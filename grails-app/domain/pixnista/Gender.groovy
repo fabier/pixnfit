e1906bfb38eaf0f002e4d069e3d30ab824a87ef8
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-25 14:28:13 +0100

    Ajout Gender

---
+++ grails-app/domain/pixnista/Gender.groovy
@@ -0,0 +1,20 @@
+package pixnista
+
+class Gender extends BaseEntity {
+
+    static constraints = {
+        name blank: false, unique: true
+    }
+
+    /**
+     * Gets users with this gender
+     */
+    List<User> getUsers() {
+        User.findAllByGender(this)
+    }
+
+    // Delete is forbidden
+    def beforeDelete() {
+        return false
+    }
+}
