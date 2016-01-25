d5ee64765141dc3f006089fcd57f5a9ff5acdea1
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-25 14:28:06 +0100

    Ajout FileExtension

---
+++ grails-app/domain/pixnista/FileExtension.groovy
@@ -0,0 +1,20 @@
+package pixnista
+
+class FileExtension extends BaseEntity {
+
+    /**
+     * File extension
+     * Ex : ".jpg", ".jpeg", ".gif"
+     */
+    String extension
+
+    static constraints = {
+        name blank: false, unique: true
+        extension blank: false, unique: true
+    }
+
+    // Delete is forbidden
+    def beforeDelete() {
+        return false
+    }
+}
