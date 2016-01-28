d02e9cf6b98d279193dbde9a1720e4882abf3b60
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-28 22:26:34 +0100

    Ajout du service de download

---
+++ grails-app/services/pixnista/DownloadService.groovy
@@ -0,0 +1,13 @@
+package pixnista
+
+import grails.transaction.Transactional
+
+@Transactional
+class DownloadService {
+
+    byte[] download(String url) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()
+        byteArrayOutputStream << new URL(url).openConnection()?.inputStream;
+        byteArrayOutputStream.toByteArray()
+    }
+}
