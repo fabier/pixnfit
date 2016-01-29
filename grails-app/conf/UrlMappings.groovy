ba4398f77e8d91e1542d24fe7c5ae081ba992256
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-29 21:35:19 +0100

    Ajout des mappings pour l'api (en cours de dev)

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,15 +1,37 @@
 class UrlMappings {
 
-	static mappings = {
-        "/$controller/$action?/$id?"{
+    static mappings = {
+
+        /**
+         * REST API MAPPINGS
+         */
+//        "/api/bodyTypes"(resources: 'bodyType')
+//        "/api/countrys"(resources: 'country')
+//        "/api/fashionStyles"(resources: 'fashionStyle')
+//        "/api/images"(resources: 'image')
+//        "/api/imageTypes"(resources: 'imageType')
+//        "/api/languages"(resources: 'language')
+//        "/api/messages"(resources: 'message')
+//        "/api/posts"(resources: 'post')
+//        "/api/postComments"(resources: 'postComment')
+//        "/api/postCommentVotes"(resources: 'postCommentVote')
+//        "/api/postVotes"(resources: 'postVote')
+//        "/api/states"(resources: 'state')
+//        "/api/users"(resources: 'user')
+//        "/api/visibilitys"(resources: 'visibility')
+//        "/api/voteReasons"(resources: 'voteReason')
+
+        /**
+         * OTHER CONTROLLERS
+         */
+        "/$controller/$action?/$id?" {
             constraints {
                 // apply constraints here
                 id(matches: /\d*/)
             }
         }
 
-
         "/"(controller: "public")
-        "500"(view:'/error')
-	}
+        "500"(view: '/error')
+    }
 }
