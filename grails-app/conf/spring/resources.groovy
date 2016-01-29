b82dab98da9440349b21485e41d1f73feae5045b
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-29 21:35:42 +0100

    Modifications pour ne remonter que certaines données pour un objet métier

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
+import grails.rest.render.xml.XmlRenderer
+import pixnista.Post
+
 // Place your Spring DSL code here
 beans = {
+//    postRenderer(XmlRenderer, Post) {
+//        includes = ['id', 'name', 'description']
+//    }
 }
