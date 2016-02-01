ee3c0e0a2dc85c400ab05a64a0a645f298a01525
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-01 15:53:51 +0100

    Aucune action n'est possible sur les webservices User
    L'action de visualisation d'un profil et de mise à jour d'un profil sera
    mis en place prochainement

--- grails-app/controllers/api/UserRestController.groovy
+++ grails-app/controllers/api/UserRestController.groovy
@@ -1,14 +1,45 @@
 package api
 
 import grails.rest.RestfulController
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
 import pixnfit.User
 
+/**
+ * On n'a que le droit que de voir la fiche pour un utilisateur donné,
+ * ou de mettre à jour la fiche d'un utilisateur,
+ * à condition d'être loggué en tant que cet utilisateur
+ */
 @Secured("hasRole('ROLE_USER')")
 class UserRestController extends RestfulController {
     static responseFormats = ['json', 'xml']
 
-    UserRestController(){
+    UserRestController() {
         super(User)
+    }
+
+    def index(Integer max) {
+        // Interdit de lister les utilisateurs depuis le webservice
+        render status: HttpStatus.FORBIDDEN
+    }
+
+    def show() {
+        // Interdit de lister les utilisateurs depuis le webservice
+        render status: HttpStatus.FORBIDDEN
+    }
+
+    def save() {
+        // Interdit de créer les utilisateurs depuis le webservice
+        render status: HttpStatus.FORBIDDEN
+    }
+
+    def update() {
+        // Interdit de mettre à jour les utilisateurs depuis le webservice
+        render status: HttpStatus.FORBIDDEN
+    }
+
+    def delete() {
+        // Interdit de supprimer les utilisateurs depuis le webservice
+        render status: HttpStatus.FORBIDDEN
     }
 }
