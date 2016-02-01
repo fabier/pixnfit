6e3fe032dc23fedd98bc689822bfb166fc9e0f2e
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-01 15:55:04 +0100

    Aucune action n'est autorisée pour les rôles, sauf l'affichage par identifiant

--- grails-app/controllers/api/RoleRestController.groovy
+++ grails-app/controllers/api/RoleRestController.groovy
@@ -3,29 +3,41 @@ package api
 import grails.rest.RestfulController
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
+import pixnfit.Role
 import pixnfit.User
 
+/**
+ * Il n'est pas autorisé de lister les roles, mais on peut voir pour un identifiant de role donné les informations de ce role
+ */
 @Secured("hasRole('ROLE_USER')")
 class RoleRestController extends RestfulController {
     static responseFormats = ['json', 'xml']
-
-    static allowedMethods = [create: 'POST']
 
     RoleRestController() {
         super(User)
     }
 
     def index(Integer max) {
-        // Interdit de lister les utilisateurs depuis le webservice
+        // Interdit de lister les roles depuis le webservice
         render status: HttpStatus.FORBIDDEN
     }
 
-    def show() {
-        // Interdit de lister les utilisateurs depuis le webservice
+    def show(Role role) {
+        respond role
+    }
+
+    def save() {
+        // Interdit de créer les roles depuis le webservice
         render status: HttpStatus.FORBIDDEN
     }
 
-    def create() {
+    def update() {
+        // Interdit de mettre à jour les roles depuis le webservice
+        render status: HttpStatus.FORBIDDEN
+    }
 
+    def delete() {
+        // Interdit de supprimer les roles depuis le webservice
+        render status: HttpStatus.FORBIDDEN
     }
 }
