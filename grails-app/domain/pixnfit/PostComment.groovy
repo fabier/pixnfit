a13d9098f9bd9df32beefbb89d6c539704d78b9a
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-01 13:47:28 +0100

    Mapping plus simple PostComment->Post

--- grails-app/domain/pixnfit/PostComment.groovy
+++ grails-app/domain/pixnfit/PostComment.groovy
@@ -3,13 +3,15 @@ package pixnfit
 class PostComment extends BaseEntity {
 
     /**
+     * The post on which this postComment is
+     */
+    Post post
+
+    /**
      * Votes on this comment
      */
     static hasMany = [postCommentVotes: PostCommentVote]
 
-    /**
-     * The post on which this comment is
-     */
     static belongsTo = [post: Post]
 
     static constraints = {
