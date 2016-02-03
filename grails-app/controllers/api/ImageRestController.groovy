19a543d343266e69c0f04f55c9e7636e45f33730
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-03 13:47:57 +0100

    Ajout de la méthode "data", permettant de récupérer les données d'une image depuis les API

--- grails-app/controllers/api/ImageRestController.groovy
+++ grails-app/controllers/api/ImageRestController.groovy
@@ -1,14 +1,18 @@
 package api
 
-import grails.rest.RestfulController
 import org.springframework.security.access.annotation.Secured
+import pixnfit.DynamicDataRestfulController
 import pixnfit.Image
 
 @Secured("hasRole('ROLE_USER')")
-class ImageRestController extends RestfulController {
-    static responseFormats = ['json', 'xml']
+class ImageRestController extends DynamicDataRestfulController {
 
     ImageRestController() {
         super(Image)
+    }
+
+    def data() {
+        Image image = Image.get(params.imageRestId)
+        respond image.imageData
     }
 }
