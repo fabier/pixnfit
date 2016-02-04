d56683e266495f1d98f21f8c6b96fbb226e79f99
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-04 10:33:43 +0100

    On passe toutes les URL au pluriel (plus standard)

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -14,32 +14,32 @@ class UrlMappings {
         // ------------------------
         // -- Static Data (READ ONLY)
         // ------------------------
-        "/api/v1/bodyType"(resources: "bodyTypeRest", includes: ["index", "show"])
-        "/api/v1/country"(resources: "countryRest", includes: ["index", "show"])
-        "/api/v1/fashionStyle"(resources: "fashionStyleRest", includes: ["index", "show"])
-        "/api/v1/gender"(resources: "genderRest", includes: ["index", "show"])
-        "/api/v1/imageType"(resources: "imageTypeRest", includes: ["index", "show"])
-        "/api/v1/language"(resources: "languageRest", includes: ["index", "show"])
-        "/api/v1/postType"(resources: "postTypeRest", includes: ["index", "show"])
-        "/api/v1/state"(resources: "stateRest", includes: ["index", "show"])
-        "/api/v1/visibility"(resources: "visibilityRest", includes: ["index", "show"])
-        "/api/v1/voteReason"(resources: "voteReasonRest", includes: ["index", "show"])
+        "/api/v1/bodyTypes"(resources: "bodyTypeRest", includes: ["index", "show"])
+        "/api/v1/countries"(resources: "countryRest", includes: ["index", "show"])
+        "/api/v1/fashionStyles"(resources: "fashionStyleRest", includes: ["index", "show"])
+        "/api/v1/genders"(resources: "genderRest", includes: ["index", "show"])
+        "/api/v1/imageTypes"(resources: "imageTypeRest", includes: ["index", "show"])
+        "/api/v1/languages"(resources: "languageRest", includes: ["index", "show"])
+        "/api/v1/postTypes"(resources: "postTypeRest", includes: ["index", "show"])
+        "/api/v1/states"(resources: "stateRest", includes: ["index", "show"])
+        "/api/v1/visibilities"(resources: "visibilityRest", includes: ["index", "show"])
+        "/api/v1/voteReasons"(resources: "voteReasonRest", includes: ["index", "show"])
 
         // ------------------------
         // -- Dynamic Data (READABLE, INSERTABLE, UPDATABLE, DELETABLE)
         // ------------------------
         // On peut visualiser des images, en créer, et récupérer les données associées
-        "/api/v1/image"(resources: "imageRest", includes: ["show", "save", "update", "delete"]) {
+        "/api/v1/images"(resources: "imageRest", includes: ["show", "save", "update", "delete"]) {
             "/data"(controller: "imageRest", action: "data", method: "GET")
         }
 
         // On peut créer des messages
-        "/api/v1/message"(resources: "messageRest", includes: ["show", "save", "update", "delete"])
+        "/api/v1/messages"(resources: "messageRest", includes: ["show", "save", "update", "delete"])
 
         // On peut voir les posts,
         // les commentaires sur ce post, poster un nouveau commentaire
         // les votes sur ce post
-        "/api/v1/post"(resources: "postRest", includes: ["show", "save", "update", "delete"]) {
+        "/api/v1/posts"(resources: "postRest", includes: ["show", "save", "update", "delete"]) {
             "/comments"(controller: "postRest", action: "comments", method: "GET")
             "/comments"(controller: "postRest", action: "addComment", method: "POST")
             "/votes"(controller: "postRest", action: "votes", method: "GET")
@@ -47,20 +47,20 @@ class UrlMappings {
         }
 
         // Liste de posts qui ont besoin d'aide et de votes
-        "/api/v1/post/help"(controller: "postRest", action: "help", method: "GET")
+        "/api/v1/posts/help"(controller: "postRest", action: "help", method: "GET")
 
         // Sélection de posts qui ont recu de l'aide
-        "/api/v1/post/featured"(controller: "postRest", action: "featured", method: "GET")
+        "/api/v1/posts/featured"(controller: "postRest", action: "featured", method: "GET")
 
         // On peut récupérer des informations sur les commentaires
         // et voter pour un commentaire
-        "/api/v1/postComment"(resources: "postCommentRest", includes: ["index", "show", "save", "update", "delete"]) {
+        "/api/v1/postComments"(resources: "postCommentRest", includes: ["index", "show", "save", "update", "delete"]) {
             "/votes"(controller: "postCommentRest", action: "votes", method: "GET")
             "/votes"(controller: "postCommentRest", action: "addVote", method: "POST")
         }
 
         // On peut récupérer des informations sur un profil utilisateur
-        "/api/v1/user"(resources: "userRest", includes: ["show", "save", "update", "delete"]) {
+        "/api/v1/users"(resources: "userRest", includes: ["show", "save", "update", "delete"]) {
             "/incomingMessages"(controller: "userRest", action: "incomingMessages", method: "GET")
             "/outgoingMessages"(controller: "userRest", action: "outgoingMessages", method: "GET")
             "/posts"(controller: "userRest", action: "posts", method: "GET")
