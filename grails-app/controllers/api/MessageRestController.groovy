674f4bfd8fae1418f2ee61a3d84b124ccc6b4787
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-04 14:20:13 +0100

    Implémentation de la sauvegarde de messages

--- grails-app/controllers/api/MessageRestController.groovy
+++ grails-app/controllers/api/MessageRestController.groovy
@@ -1,8 +1,12 @@
 package api
 
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
 import pixnfit.DynamicDataRestfulController
 import pixnfit.Message
+import pixnfit.User
 
 @Secured("hasRole('ROLE_USER')")
 class MessageRestController extends DynamicDataRestfulController {
@@ -10,7 +14,29 @@ class MessageRestController extends DynamicDataRestfulController {
     // On ne peut qu'ajouter des messages, pas de mise à jour ou de suppression
     static allowedMethods = [save: "POST"]
 
+    SpringSecurityService springSecurityService
+
     MessageRestController() {
         super(Message)
+    }
+
+    @Override
+    @Transactional
+    def save() {
+        def json = request.JSON
+
+        // Création du message
+        Message message = new Message(
+                description: json.description,
+                recipient: User.get(json.recipientId),
+                creator: springSecurityService.currentUser
+        )
+
+        if (message.validate()) {
+            message.save()
+            respond message, [status: HttpStatus.CREATED]
+        } else {
+            respond message, [status: HttpStatus.UNPROCESSABLE_ENTITY]
+        }
     }
 }
