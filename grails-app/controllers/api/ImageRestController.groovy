38a67636d19ee00dcdf3eaf166b08b67b97ff540
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-04 16:00:04 +0100

    Implémentation de la méthode "save" et "update" pour une Image

--- grails-app/controllers/api/ImageRestController.groovy
+++ grails-app/controllers/api/ImageRestController.groovy
@@ -1,14 +1,50 @@
 package api
 
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
-import pixnfit.DynamicDataRestfulController
-import pixnfit.Image
+import pixnfit.*
 
 @Secured("hasRole('ROLE_USER')")
 class ImageRestController extends DynamicDataRestfulController {
 
+    SpringSecurityService springSecurityService
+    ImageTypeService imageTypeService
+
     ImageRestController() {
         super(Image)
+    }
+
+    @Override
+    @Transactional
+    Object save() {
+        def json = request.JSON
+        User user = springSecurityService.currentUser
+        Image image = new Image(
+                creator: user,
+                imageType: imageTypeService.jpeg()
+        )
+        bindData(image, json, [include: ['name', 'description', 'filename', 'height', 'width']])
+        foreignKeyBindDataIfNotNull(image, json, [imageType: ImageType])
+
+        if (image.validate()) {
+            image.save()
+            respond image, [status: HttpStatus.CREATED]
+        } else {
+            respond image, [status: HttpStatus.UNPROCESSABLE_ENTITY]
+        }
+    }
+
+    @Override
+    @Transactional
+    def update() {
+        def json = request.JSON
+        Image image = Post.get(params.id)
+        bindData(image, json, [include: ['name', 'description', 'filename', 'height', 'width']])
+        foreignKeyBindDataIfNotNull(image, json, [imageType: ImageType])
+        image.save()
+        respond image
     }
 
     def data() {
