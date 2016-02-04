922053de9347c10282df69e310cca6dcec13bb12
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-04 15:58:47 +0100

    Implémentation des méthodes "votes" et "addVote"

--- grails-app/controllers/api/PostCommentRestController.groovy
+++ grails-app/controllers/api/PostCommentRestController.groovy
@@ -1,16 +1,40 @@
 package api
 
+import grails.plugin.springsecurity.SpringSecurityService
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
-import pixnfit.DynamicDataRestfulController
-import pixnfit.PostComment
+import pixnfit.*
 
 @Secured("hasRole('ROLE_USER')")
 class PostCommentRestController extends DynamicDataRestfulController {
 
-    // On ne peut qu'ajouter des commentaires sur des posts, pas de mise à jour ou de suppression
-    static allowedMethods = [save: "POST"]
+    SpringSecurityService springSecurityService
 
     PostCommentRestController() {
         super(PostComment)
+    }
+
+    def votes() {
+        PostComment postComment = PostComment.get(params.postCommentRestId)
+        respond postComment.postCommentVotes?.toArray()
+    }
+
+    def addVote() {
+        def json = request.JSON
+        PostComment postComment = PostComment.get(params.postCommentRestId)
+
+        // Création du vote
+        PostCommentVote postCommentVote = new PostCommentVote(
+                postComment: postComment,
+                creator: springSecurityService.currentUser
+        )
+        bindData(postComment, json, [include: ['vote', 'description']])
+
+        if (postCommentVote.validate()) {
+            postCommentVote.save()
+            respond postCommentVote, [status: HttpStatus.CREATED]
+        } else {
+            respond postCommentVote, [status: HttpStatus.UNPROCESSABLE_ENTITY]
+        }
     }
 }
