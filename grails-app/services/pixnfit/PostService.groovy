8206c4210d8062f04388ccc27b8246e1ee8c64ea
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-11 15:11:48 +0100

    Renommage de méthodes

--- grails-app/services/pixnfit/PostService.groovy
+++ grails-app/services/pixnfit/PostService.groovy
@@ -7,34 +7,34 @@ class PostService {
 
     PostTypeService postTypeService
 
-    List<Post> mostRecentPosts(int count = 10) {
-        Post.list([max: count, sort: "dateCreated", order: "desc"])
+    List<Post> getMostRecentPosts(int max = 10) {
+        Post.list([max: max, sort: "dateCreated", order: "desc"])
     }
 
-    List<Post> mostRecentHelpPosts(int count = 10) {
-        Post.findAll(sort: "dateCreated", order: "desc", max: count) {
+    List<Post> getMostRecentHelpPosts(int max = 10) {
+        Post.findAll(sort: "dateCreated", order: "desc", max: max) {
             eq "postType", postTypeService.help()
         }
     }
 
-    List<Post> mostRecentDressingPosts(int count = 10) {
-        Post.findAll(sort: "dateCreated", order: "desc", max: count) {
+    List<Post> getMostRecentDressingPosts(int max = 10) {
+        Post.findAll(sort: "dateCreated", order: "desc", max: max) {
             eq "postType", postTypeService.dressing()
         }
     }
 
-    List<Post> getRandomHelpPosts(int count = 10) {
+    List<Post> getRandomHelpPosts(int max = 10) {
         Post.withCriteria {
             eq "postType", postTypeService.help()
-            maxResults count
+            maxResults max
             sqlRestriction " 1=1 ORDER BY random()"
         }
     }
 
-    List<Post> getRandomDressingPosts(int count = 10) {
+    List<Post> getRandomDressingPosts(int max = 10) {
         Post.withCriteria {
             eq "postType", postTypeService.dressing()
-            maxResults count
+            maxResults max
             sqlRestriction " 1=1 ORDER BY random()"
         }
     }
