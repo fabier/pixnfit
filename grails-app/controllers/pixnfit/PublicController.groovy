aa4f98f7e244458f42104ce30265e32363b8b1c4
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-11 15:16:41 +0100

    Redirection vers la home si l'utilisateur est loggué.
    On affiche la landing page dans le cas où l'utilisateur n'est pas loggué

--- grails-app/controllers/pixnfit/PublicController.groovy
+++ grails-app/controllers/pixnfit/PublicController.groovy
@@ -1,19 +1,30 @@
 package pixnfit
 
+import grails.plugin.springsecurity.SpringSecurityService
 import org.springframework.security.access.annotation.Secured
 
 @Secured(['permitAll'])
 class PublicController {
 
     static defaultAction = "index"
+
+    SpringSecurityService springSecurityService
     PostService postService
 
     def index() {
-        List<Post> helpPosts = postService.getRandomHelpPosts(12)
-        List<Post> dressingPosts = postService.getRandomDressingPosts(24)
-        render view: "home", model: [
-                helpPosts    : helpPosts,
-                dressingPosts: dressingPosts
-        ]
+        User user = springSecurityService.currentUser
+        if (user == null) {
+            // Utilisateur non connecté, on affiche la landingPage
+            render view: "landingpage"
+        } else {
+            // Utilisateur connecté, on affiche la home
+//            List<Post> helpPosts = postService.getRandomHelpPosts(36)
+            List<Post> helpPosts = postService.getMostRecentHelpPosts(36)
+//            List<Post> dressingPosts = postService.getRandomDressingPosts(24)
+
+            render view: "home", model: [
+                    helpPosts    : helpPosts
+            ]
+        }
     }
 }
