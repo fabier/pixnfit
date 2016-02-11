503f7c85d70ba1dbf851ac7fe9f0f6c70518bdda
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-11 15:19:46 +0100

    Gestion des paramètres width et height pour l'affichage de l'image

--- grails-app/controllers/pixnfit/ImageController.groovy
+++ grails-app/controllers/pixnfit/ImageController.groovy
@@ -2,14 +2,41 @@ package pixnfit
 
 import org.springframework.security.access.annotation.Secured
 
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
 @Secured("permitAll")
 class ImageController {
+
+    ImageService imageService
 
     def show(long id) {
         Image image = Image.get(id)
         if (image != null) {
-            render file: image.imageData?.data, contentType: image.imageData?.imageType?.defaultMimeType?.mimetype
+            ImageData imageData = image.imageData
+            if (imageData != null) {
+                byte[] data = imageData.data
+                if (data != null) {
+                    Integer width = params.width ? Integer.parseInt(params.width) : null
+                    Integer height = params.height ? Integer.parseInt(params.height) : null
+                    if (width != null && height != null) {
+                        // Rescale image
+                        BufferedImage bufferedImage = ImageIO.read(ImageIO.createImageInputStream(new ByteArrayInputStream(data)))
+                        BufferedImage rescaled = imageService.getScaledImageCropCentered(bufferedImage, width, height)
+                        render file: imageService.getDataAsJPEG(rescaled, 0.8f), contentType: "image/jpeg"
+                    } else {
+                        render file: data, contentType: imageData?.imageType?.defaultMimeType?.mimetype
+                    }
+                } else {
+                    // Dans tous les autres cas, erreur 404
+                    response.sendError(404)
+                }
+            } else {
+                // Dans tous les autres cas, erreur 404
+                response.sendError(404)
+            }
         } else {
+            // Dans tous les autres cas, erreur 404
             response.sendError(404)
         }
     }
