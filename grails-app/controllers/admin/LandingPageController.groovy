9c5e36abdbd49a5db1cc4a4e86118c771df8da38
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-15 16:19:09 +0100

    Ajout du message lors de l'enregistrement de l'utilisateur sur la page d'accueil

--- grails-app/controllers/admin/LandingPageController.groovy
+++ grails-app/controllers/admin/LandingPageController.groovy
@@ -5,11 +5,44 @@ import org.springframework.security.access.annotation.Secured
 @Secured("permitAll")
 class LandingPageController {
 
+    def mailService
+
     def submitEmail() {
         String email = params.email
         LandingPageEmail landingPageEmail = new LandingPageEmail(email: email)
         if (landingPageEmail.validate()) {
             landingPageEmail.save()
+
+            mailService.sendMail {
+                to landingPageEmail.email
+                from "noreply@pixnfit.com"
+                subject "PixnFit - You are on the waiting list !"
+                html "Hi !<br/>" +
+                        "<br/>" +
+                        "You’ve been added to the <b>PixnFit waiting list !!!</b><br/>" +
+                        "<br/>" +
+                        "Thank you for your subscription. Pixnfit is on an early stage, we are working on the app to provide you amazing functionalities.<br/>" +
+                        "You will be notified by email soon when the app is ready, to get an early access.<br/>" +
+                        "<br/>" +
+                        "Please do not respond to this automatic email<br/>" +
+                        "<br/>" +
+                        "Pixnfit Team."
+            }
+
+            mailService.sendMail {
+                to "contact@pixnfit.com"
+                from "noreply@pixnfit.com"
+                subject "PixnFit - You are on the waiting list !"
+                html "A new user has submitted its email on PixnFit !" +
+                        "<br/>" +
+                        "Email : <b>$landingPageEmail.email</b><br/>" +
+                        "Total emails registered : <b>${LandingPageEmail.count()}</b><br/>" +
+                        "<br/>" +
+                        "Please do not respond to this automatic email<br/>" +
+                        "<br/>" +
+                        "Pixnfit Team."
+            }
+
             flash.message = "Your email has been successfully submitted.<br/>We'll catch up to you soon !"
             redirect controller: "public", action: "index"
         } else {
