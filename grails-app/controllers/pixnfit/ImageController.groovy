003ff64611eaefd3db85e08f7fdd350479eae3fa
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-19 14:26:52 +0100

    Dans les paramètres d'appel des images, on ne peut pas mettre width > 1024 ni height > 1024

--- grails-app/controllers/pixnfit/ImageController.groovy
+++ grails-app/controllers/pixnfit/ImageController.groovy
@@ -12,28 +12,29 @@ class ImageController {
     private static final boolean ENABLE_IMAGE_CACHE = true
     private static final Map<String, Object> IMAGE_CACHE = Collections.synchronizedMap(new LRUCache<>(100))
 
+    private static final MAX_WIDTH = 1024;
+    private static final MAX_HEIGHT = 1024;
+
     def show(long id) {
-        Image image = Image.get(id)
-        Integer width = params.width ? Integer.parseInt(params.width) : null
-        Integer height = params.height ? Integer.parseInt(params.height) : null
+        int width = params.width ? Math.max(0, Math.min(MAX_WIDTH, Integer.parseInt(params.width))) : 0
+        int height = params.height ? Math.max(0, Math.min(MAX_HEIGHT, Integer.parseInt(params.height))) : 0
 
         String key = "${id}|${width}|${height}"
 
         if (ENABLE_IMAGE_CACHE) {
-            synchronized (IMAGE_CACHE) {
-                if (IMAGE_CACHE.containsKey(key)) {
-                    render file: IMAGE_CACHE.get(key), contentType: "image/jpeg"
-                    return
-                }
+            if (IMAGE_CACHE.containsKey(key)) {
+                render file: IMAGE_CACHE.get(key), contentType: "image/jpeg"
+                return
             }
         }
 
+        Image image = Image.get(id)
         if (image != null) {
             ImageData imageData = image.imageData
             if (imageData != null) {
                 byte[] data = imageData.data
                 if (data != null) {
-                    if (width != null && height != null) {
+                    if (width > 0 && height > 0) {
                         // Rescale image
                         BufferedImage rescaled = imageService.getScaledImageCropCenteredAndKeepOrientationUsingEXIFMetadata(data, width, height)
                         byte[] binaryData = imageService.getDataAsJPEG(rescaled)
@@ -42,9 +43,7 @@ class ImageController {
                         }
                         render file: binaryData, contentType: "image/jpeg"
                     } else {
-                        if (ENABLE_IMAGE_CACHE) {
-                            IMAGE_CACHE.put(key, data)
-                        }
+                        // On ne met pas les images fullscreen en cache, ca prend trop de mémoire
                         render file: data, contentType: imageData?.imageType?.defaultMimeType?.mimetype
                     }
                 } else {
